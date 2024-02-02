59ffd242c1c213c73366b322a59dd4749b940f06
Avinash Patel <avinash.patel@etatvasoft.com>
2024-02-02 18:02:27 +0530

    MVC

---
+++ MVC/HelloDoc/Controllers/PatientController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloDoc.Controllers
+{
+    public class PatientController : Controller
+    {
+        [Route("/")]
+        public IActionResult PatientSite()
+        {
+            return View();
+        }
+
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitRequest()
+        {
+            return View();
+        }
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        public IActionResult PatientRequest()
+        {
+            return View();
+        }
+        public IActionResult FamilyFriendRequest()
+        {
+            return View();
+        }
+
+        public IActionResult ConciergeRequest()
+        {
+            return View();
+        }
+
+        public IActionResult BusinessRequest()
+        {
+            return View();
+        }
+    }
+}
